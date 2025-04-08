2f6fd0d829fc6a2b6213c8266d2524cd643f43ff
Pedro Linero <xplinari721@ieshnosmachado.org>
2025-04-08 08:57:56 +0200

    Primer commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  </StrictMode>,
+)
